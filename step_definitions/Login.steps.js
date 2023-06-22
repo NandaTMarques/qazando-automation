dbdb7c352b7bacdaef98b34b98ba4ea4c56d1d02
NandaTMarques <nandateixmarques@gmail.com>
2023-06-22 15:42:26 -0300

    Ultimo commit

--- step_definitions/Login.steps.js
+++ step_definitions/Login.steps.js
@@ -2,52 +2,52 @@ const { I } = inject();
 const LoginAction = require('../page_definitions/page_actions/LoginAction.js');
 
 
-Given('que o usuário esteja na tela de login', async () => {
-  await LoginAction.seeLoginPage();
+Given('que o usuário esteja na tela de login', () => {
+  LoginAction.seeLoginPage();
 });
 
-When('ele preenche os campos de e-mail e senha com credenciais validas', async () => {
-  await LoginAction.fillLogin();
+When('ele preenche os campos de e-mail e senha com credenciais validas', () => {
+  LoginAction.fillLogin();
 });
 
-When('aciona a opção para logar', async () => {
-  await LoginAction.enterLogin();
+When('aciona a opção para logar', () => {
+  LoginAction.enterLogin();
 });
 
-Then('ele acessará a home do sistema', async () => {
-  await LoginAction.seeHome();
-});
-
-
-Given(/^que o usuário esteja na tela de login$/, async () => {
-  await LoginAction.seeLoginPage();
-});
-
-When(/^ele preenche os campos de e-mail inválido e senha valida$/, async () => {
-	await LoginAction.fillLoginErrorEmail();
-});
-
-When(/^aciona a opção para logar$/, async () => {
-  await LoginAction.enterLogin();
-});
-
-Then(/^ele verá uma mensagem de erro no login$/, async () => {
-  await LoginAction.messageLoginFailed();
+Then('ele acessará a home do sistema', () => {
+  LoginAction.seeHome();
 });
 
 
-Given(/^que o usuário esteja na tela de login$/, async () => {
-  await LoginAction.seeLoginPage();
+Given(/^que o usuário esteja na tela de login$/, () => {
+  LoginAction.seeLoginPage();
 });
 
-When(/^ele preenche os campos de e-mail válido e senha inválida$/, async () => {
-	await LoginAction.fillLoginErrorSenha();
+When(/^ele preenche os campos de e-mail inválido e senha valida$/, () => {
+	LoginAction.fillLoginErrorEmail();
 });
 
-When(/^aciona a opção para logar$/, async () => {
-  await LoginAction.enterLogin();
+When(/^aciona a opção para logar$/, () => {
+  LoginAction.enterLogin();
 });
 
-Then(/^ele verá uma mensagem de erro no login$/, async () => {
-	await LoginAction.messageLoginFailed();
+Then(/^ele verá uma mensagem de erro no login$/, () => {
+  LoginAction.messageLoginFailed();
+});
+
+
+Given(/^que o usuário esteja na tela de login$/, () => {
+  LoginAction.seeLoginPage();
+});
+
+When(/^ele preenche os campos de e-mail válido e senha inválida$/, () => {
+	LoginAction.fillLoginErrorSenha();
+});
+
+When(/^aciona a opção para logar$/, () => {
+  LoginAction.enterLogin();
+});
+
+Then(/^ele verá uma mensagem de erro no login$/, () => {
+	LoginAction.messageLoginFailed();
 });
